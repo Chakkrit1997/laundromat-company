95b4277c6d3e5d4ed7ec3453cff265bf892602ae
Chakkrit <guitar_dark_soft@hotmail.com>
2023-07-02 01:41:03 +0700

    [Edited] migration washing

--- database/migrations/1688155138041_washings.ts
+++ database/migrations/1688155138041_washings.ts
@@ -1,13 +1,13 @@
 import BaseSchema from '@ioc:Adonis/Lucid/Schema'
 
 export default class extends BaseSchema {
-  protected tableName = 'machines'
+  protected tableName = 'washings'
 
   public async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
-      table.boolean('is_active').defaultTo(false)
-      table.dateTime('complete_at').defaultTo(null)
+      table.integer('machine_number')
+      table.datetime('complete_at')
       table.dateTime('created_at', { useTz: true })
       table.dateTime('updated_at', { useTz: true })
     })
